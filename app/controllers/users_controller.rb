2a608775076554b8d0e5ed296b3aade2c215f86e
RyoyaOzaki <r.ozaki3018@gmail.com>
2018-11-11 14:43:43 +0900

    Completed adding function for user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to root_path, updated: "アカウントが更新されました"
     else
       render :edit
     end
