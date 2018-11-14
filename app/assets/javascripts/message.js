a9e60abdc1815074982ed4d6aef70c90487a2386
RyoyaOzaki <r.ozaki3018@gmail.com>
2018-11-14 21:58:35 +0900

    Updating-for-Ajax
    Modified per mentor’s advise

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,26 @@
 $(function (){
   function buildHTML(message){
-    var html =
-      `<div class="message">${message.content}</div>`
+     var html =
+      `<div class="chat-main__body">
+        <div class="chat-main__body--messages-list">
+          <div class="chat-main__message clearfix" data-id="2096">
+            <div class="chat-main__message-name">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__message-time">
+              ${message.created_at}
+            </div>
+            <div class="chat-main__message-body">
+              <p class="chat-main__message__content">
+                ${message.content}
+              </p>
+              <div class="chat-main__message__image">
+                ${message.img}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
     return html;
   };
 
@@ -9,6 +28,7 @@ $(function (){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+    console.log("jsjsjs")
     $.ajax({
       url: url,
       type: "POST",
